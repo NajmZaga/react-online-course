f257ec06a00d8ba8d9e4888918b72f7636d77570
NajmEddine <najmeddine.zaga@symdrik.com>
2020-05-20 22:37:21 +0100

    handling events & onditionnal rendering

--- src/App.js
+++ src/App.js
@@ -1,67 +1,29 @@
 import React, { Component } from 'react';
-import ClassCompo from './ClassCompo';
 
 // Component listens to state & props update to re-render
 export default class App extends Component {
+
+  // React is a One-Way data binding !== Two ways data binding
+
   constructor() {
     super();
     this.state = {
-      bookTitle: 'Origin',
-      bookAuthor: 'Dan Brown',
-      bookPrice: '20$',
-      otherBook: {
-        bookTitle: 'The lost symbol',
-        bookPrice: '40$'
-      },
-      toggleShowChild: false
+      isVisible: true
     }
   }
 
-  handleChangeBookTitle = () => {
+  handleToggleShow = () => {
     this.setState({
-      ...this.state,
-      bookAuthor: 'Haithem',
-      otherBook: {
-        ...this.state.otherBook,
-        bookTitle: '1000$',
-        date: 'sdfsdfsdfsdfsd'
-      }
-    });
-  }
-
-  handleSwitch = () => {
-    this.setState({
-      ...this.state,
-      toggleShowChild: !this.state.toggleShowChild
-    });
-  }
-
-  // Component lifecycle
-
-  componentWillUpdate() {
-    // Do something before the component is updated (updates always listens to state and props changing)
-    console.log("-------> Component will update - 3 -");
-  }
-
-  componentDidUpdate() {
-    // Do something once the component is updated (updates always listens to state and props changing)
-    console.log("-------> Component did update - 4 -");
+      isVisible: !this.state.isVisible
+    })
   }
 
   render() {
     return (
       <div>
+        <button onClick={ this.handleToggleShow }>{ this.state.isVisible ? 'Hide' : 'Show' } image</button>
         <br />
-        The book <b>{ this.state.bookTitle }</b> is written by <b>{ this.state.bookAuthor }</b> and it costs <b>{ this.state.bookPrice }</b>
-        <br />
-        { this.state.bookAuthor } has also wrote { this.state.otherBook.date }
-        <br />
-        <button onClick={ this.handleChangeBookTitle }>Change book title</button>
-        <br />
-        <button onClick={ this.handleSwitch }>Show / Hide</button>
-        {
-          this.state.toggleShowChild && <ClassCompo />
-        }
+        { this.state.isVisible && <img src="https://cdn.pixabay.com/photo/2015/12/01/20/28/road-1072823__340.jpg" width="70%" alt="" /> }
       </div>
     )
   }
