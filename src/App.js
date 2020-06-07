f824cddc41593d3a33db5e23e2b984cf53282c9e
NajmZaga <najmeddine.zaga.com>
2020-06-07 12:24:23 +0100

    react project folder structure

--- src/App.js
+++ src/App.js
@@ -1,34 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
-import { FuncCompo } from './components/FuncCompo';
 
 export const App = (props) => {
-
-  //    count    - seCount
-  //    variable - function (setState)
-  //    [variable, setVariable]
-  const [count, setCount] = useState(0);
-  const [toggle, setToggle] = useState(false);
-
-  console.log(props);
-
-  useEffect(() => {
-    increment();
-  }, [toggle]);
-
-  const increment = () => {
-    setCount(count + 1);
-  };
-
-  const toggler = () => {
-    setToggle(!toggle);
-  };
-  
   return (
     <div>
-      <h1>count: { count }</h1>
-      <button onClick={ increment }>Increment</button>
-      <button onClick={ toggler }>Toggle</button>
     </div>
   )
 }
